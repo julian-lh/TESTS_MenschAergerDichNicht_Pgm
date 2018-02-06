9061fa34243fa8e20d042e763ba1dd0aa1c311cb
Julio <j.lopesh@yahoo.com>
2018-02-06 01:35:05 +0100

    Neue Tests

---
+++ GameLoopTemplate.py
@@ -0,0 +1,42 @@
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+	
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			crashed = True
+		
+		
+		
+	pygame.display.update()
+	clock.tick(60)
+
+pygame.quit()
+quit()
+
+
+
+
+
+
+
+
+# Notizen
+# Tupel: 800,600 oder (800,600)
+
+#print(event)
